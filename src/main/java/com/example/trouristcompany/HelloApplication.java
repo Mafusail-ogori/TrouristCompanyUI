2ac026e880b7fd3919b3639f16a382fc7b4d4a49
Mafusaillo <danylo.prots.kn.2021@lpnu.ua>
2022-11-26 00:52:36 +0200

    TouristCompanyUI

--- src/main/java/com/example/trouristcompany/HelloApplication.java
+++ src/main/java/com/example/trouristcompany/HelloApplication.java
@@ -2,17 +2,25 @@ package com.example.trouristcompany;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("touristCompany.fxml")));
+        Scene scene = new Scene(root);
+        stage.setTitle("Tourist company application");
+        stage.getIcons().add(new Image("logo.png"));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.trouristcompany {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens com.example.trouristcompany to javafx.fxml;
     exports com.example.trouristcompany;
 }
