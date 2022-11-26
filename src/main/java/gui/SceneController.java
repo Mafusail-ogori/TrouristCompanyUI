a2c13c0d962cb540134119976eb5b4240f2056cd
Mafusaillo <danylo.prots.kn.2021@lpnu.ua>
2022-11-26 20:30:18 +0200

    TouristCompanyUI

--- src/main/java/gui/SceneController.java
+++ src/main/java/gui/SceneController.java
@@ -16,16 +16,19 @@ public class SceneController {
     protected Parent root;
 
     public void switchToStarterScene(ActionEvent actionEvent) throws IOException {
-        this.root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Starter.fxml")));
-        this.stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        this.scene = new Scene(root);
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("starter.fxml")));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
 
     public void switchToLoggingScene(ActionEvent actionEvent) throws IOException {
-        this.root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("entrance.fxml")));
-        this.stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        this.scene = new Scene(root);
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("entrance.fxml")));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
+
 }
